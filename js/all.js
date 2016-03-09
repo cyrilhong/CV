d508c314a02284edabc0576e7a178a41613615f9
Cyril Hong <cyrilhong@gmail.com>
2016-03-09 22:55:19 +1100

    v1

---
+++ js/all.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	$('.menu ul').hide();
+    $('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	});
+    $(".mobile li").click(function(){
+	    $(".menu ul").toggle();
+	});
+});
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('actived');
+        })
+        $(this).addClass('actived');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#menu-center a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.menu ul li a').removeClass("actived");
+            currLink.addClass("actived");
+        }
+        else{
+            currLink.removeClass("actived");
+        }
+    });
+}
